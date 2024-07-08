0377ae9cbe6f77f8f93632b9e3ca9202114121b7
alan shama <antonyrubes@gmail.com>
2024-07-08 17:57:25 +0530

    Initial changes for AI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
+import { useState } from 'react'; 
 import './App.css';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 function App() {
+  const [state, setState] = useState ("");
+  const [responseAI, setResponseAI] = useState ("");
+
+  const genAI = new GoogleGenerativeAI("AIzaSyD-lBAoM816guG_u4mg0Y2MTHKDonz_CpM");
+  
+  
+  const model=genAI.getGenerativeModel({
+    model: "gemini-1.5-pro",
+  });
+
+  async function response (value) {
+    const res = await model.generateContent(value);
+    setResponseAI(res.response.text());
+    return res;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tony AI
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <span>{responseAI}</span>
+      </div>
+      <input type='text' onBlur={(e)=>{setState(e.target.value)}}></input>
+
+      <input type='submit' onClick={response(state)}></input>
     </div>
   );
 }
